91e85a5648d3c93ecf1a0737bba6e3a23c4f1df3
nobdefender <v1234198@gmail.com>
2021-02-10 21:48:06 +0300

    Initial commit

---
+++ src/app.js
@@ -0,0 +1,27 @@
+const express = require('express')
+const app = express();
+require('dotenv').config()
+const PORT = process.env.PORT || 3001;
+const authRoute = require('./routes/auth')
+const session = require('express-session')
+const passport = require('passport')
+require('./strategies/discordstrategy')
+
+
+app.use(session({
+    secret: 'some random secret',
+    cookie: {
+        maxAge: 60000*60*24
+    },
+    saveUninitialized: false,
+    resave: false,
+}))
+
+app.use('/auth', authRoute)
+
+app.use(passport.initialize())
+app.use(passport.session());
+
+app.listen(PORT, () => {
+    console.log(`listening on port ${PORT}`)
+})
